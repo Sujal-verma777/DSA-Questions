20f2e32267b2bdc3a236a36afa47f598edd4481d
Sujal Verma <sujalv124@gmail.com>
2026-05-04 23:56:07 +0530

    Add files via upload

---
+++ Day-14-Modules & Libraries.py
@@ -0,0 +1,24 @@
+# What is a Module?
+# A module = file containing Python code
+
+# What is a Library?
+# A library = collection of modules
+
+# mymodule.py
+def greet():
+    print("Hello Sujal")
+import mymodule
+mymodule.greet()
+# output: Hello Sujal
+
+
+# Using built-in math module
+import math
+print(math.sqrt(16))
+print(math.factorial(5))
+print(math.pi)
+# output: 4.0
+#         120 
+#         3.141592653589793
+
+
