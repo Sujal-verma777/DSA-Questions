caf5ec1bb28e975a1c99a95390e839973367876e
Sujal Verma <sujalv124@gmail.com>
2026-05-05 23:59:55 +0530

    Create DAY-15-Mini Project Day.py

---
+++ DAY-15-Mini Project Day.py
@@ -0,0 +1,20 @@
+# Day 15 - Mini Project (Student Management System)
+
+## Features:
+
+# * Add Student
+# * View Students
+# * Search Student
+# * Delete Student
+
+## Concepts Used:
+
+# * Lists
+# * Dictionary
+# * Functions
+# * Loops
+# * Conditions
+
+## Learning:
+
+# This project helped me understand how to build a real application using Python basics.
